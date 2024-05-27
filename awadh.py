674e7c29f846a4dc70bbeaf0d210138e5b664fd0
awadh <awadhkumarkumar421@gmail.com>
2024-05-27 12:26:29 +0000

    aaaaa

---
+++ awadh.py
@@ -0,0 +1,10 @@
+# list =eval(input("enter list: "))
+# print (list)
+
+
+# list = ['you creat your own reality']
+# print("list with the use of string: ")
+# print(list)
+
+# vowellist = ['a','e','i','o','u']
+# print(list(vowellist))
